76338e79c4cb7042823b64d43b4b51fd04fb4577
RaidenH-UWT <raidenh@uw.edu>
2026-04-18 13:05:15 -0700

    Fix formatting

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -51,8 +51,12 @@ export const getMovies = async (request: Request, response: Response) => {
                     keywords.length > 0
                         ? keywords.every(
                               (word) =>
-                                  (movie.title as string).toUpperCase().indexOf(word.toUpperCase()) > -1 ||
-                                  (movie.overview as string).toUpperCase().indexOf(word.toUpperCase()) > -1
+                                  (movie.title as string)
+                                      .toUpperCase()
+                                      .indexOf(word.toUpperCase()) > -1 ||
+                                  (movie.overview as string)
+                                      .toUpperCase()
+                                      .indexOf(word.toUpperCase()) > -1
                           )
                         : true
                 )
