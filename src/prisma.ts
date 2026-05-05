41b097e221247cdc6aa6c8084b780a21eeea62cc
RaidenH-UWT <raidenh@uw.edu>
2026-05-05 15:59:58 -0700

    format

--- src/prisma.ts
+++ src/prisma.ts
@@ -4,7 +4,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL! + "?sslmode=disable",
+    connectionString: process.env.DATABASE_URL! + '?sslmode=disable',
 });
 const adapter = new PrismaPg(pool);
 
